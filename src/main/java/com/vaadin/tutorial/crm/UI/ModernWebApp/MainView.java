157eee5a3da3f267d5bd06a2903e70a8418363c3
Meqdad <meghdadd78@gmail.com>
2020-10-07 23:40:32 +0330

    MainView created and primary GridView added

---
+++ src/main/java/com/vaadin/tutorial/crm/UI/ModernWebApp/MainView.java
@@ -0,0 +1,54 @@
+package com.vaadin.tutorial.crm.UI.ModernWebApp;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.Backend.Model.Company;
+import com.vaadin.tutorial.crm.Backend.Model.Contact;
+import com.vaadin.tutorial.crm.Backend.Service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route(value = "app")
+public class MainView extends VerticalLayout {
+
+    private ContactService contactService;
+    private Grid<Contact> grid = new Grid<>(Contact.class);
+    private TextField filterText = new TextField();
+
+    @Autowired
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureFilter();
+        configureGrid();
+        add(filterText, grid);
+        updateList();
+    }
+
+    private void configureFilter() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
+    }
+
+}
